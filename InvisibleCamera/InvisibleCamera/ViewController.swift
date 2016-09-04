7d5b1e99aa19852919d6382b14b2ad17ea81f564
tosada <tomoya.a.osada@gmail.com>
2016-09-05 01:51:10 +0900

    commit

---
+++ InvisibleCamera/InvisibleCamera/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  InvisibleCamera
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 一旦ボタン作る
+    }
+
+    override func viewWillAppear(animated: Bool){
+        // スクリーン設定をする
+        
+        // カメラ設定をする
+    }
+    
+    
+    
+    func setupDisplay(){
+        // スクリーン設定
+    }
+    
+    
+    func setupCamera(){
+        // カメラ設定
+    }
+    
+    func tappdShutterButton(){
+        //ボタン押した時の設定
+        
+    }
+    
+    
+    // メモリ解放する
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
