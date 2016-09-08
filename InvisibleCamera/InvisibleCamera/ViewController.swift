b25b7bd60b862e4c39b9a4f8f98f4fdbdddd5933
tosada <tomoya.a.osada@gmail.com>
2016-09-08 13:00:02 +0900

    add underview

--- InvisibleCamera/InvisibleCamera/ViewController.swift
+++ InvisibleCamera/InvisibleCamera/ViewController.swift
@@ -10,6 +10,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // 一旦ボタン作る
+        
+        let underView = UIView(frame: CGRect(origin: CGPointZero, size:  CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height/8)))
+        underView.center = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-underView.frame.size.height/2)
+        underView.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.4)
+        self.view.addSubview(underView)
+        
+        let shutterButton = UIButton(frame: CGRect(origin: CGPointZero, size: CGSize(width: underView.frame.size.height-15, height: underView.frame.size.height-15)))
+        shutterButton.center = CGPoint(x: underView.frame.size.width/2, y: underView.frame.size.height/2)
+        underView.addSubview(shutterButton)
+        
+        
+        
     }
 
     override func viewWillAppear(animated: Bool){
